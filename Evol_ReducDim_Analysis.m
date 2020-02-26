dfaa123cffc4e470fc0594398c76b34e9a5180dd
Animadversio <binxu.wang@wustl.edu>
2020-02-26 16:23:17 -0600

    add notes

--- Evol_ReducDim_Analysis.m
+++ Evol_ReducDim_Analysis.m
@@ -1,6 +1,6 @@
 %%
 Set_Path;
-expftr = contains(ExpSpecTable_Aug.expControlFN,"200225");
+expftr = contains(ExpSpecTable_Aug.expControlFN,"200226");
 % expftr = contains(ExpSpecTable_Aug.expControlFN,"generate") & ...
 %      contains(ExpSpecTable_Aug.Exp_collection, "Optimizer_cmp");
 %  ExpSpecTable_Aug.Expi<=5 & ExpSpecTable_Aug.Expi>=4 & ...
