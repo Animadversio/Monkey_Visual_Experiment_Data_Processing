87440732c1b875338c11820d411d3cd1a0320f6d
Animadversio <binxu.wang@wustl.edu>
2020-03-10 22:10:40 -0500

    add latent code loading

---
+++ utils/load_codes_all.m
@@ -0,0 +1,18 @@
+function [codes_all, img_ids] = load_codes_all(stim_path, threadi)
+% stim_path = "N:\Stimuli\2019-12-Evolutions\2020-03-10-Alfa-01\2020-03-10-13-50-57";
+% threadi = 1;
+if nargin == 1
+    threadi = 1;
+end
+data_fn  = ls(fullfile(stim_path, sprintf("*_thread%03d_code.mat", threadi - 1)));
+data_fn = sort(string(data_fn)); 
+codes_all = [];
+img_ids = {};
+for block_k = 1:length(data_fn)
+D = load(fullfile(stim_path, data_fn{block_k}),"codes", "ids");
+assert(contains(data_fn{block_k}, sprintf("block%03d_thread%03d_code.mat", block_k, threadi - 1)))
+codes_all = [codes_all; D.codes];
+img_ids = [img_ids, D.ids];
+end
+img_ids = string(img_ids);
+end
