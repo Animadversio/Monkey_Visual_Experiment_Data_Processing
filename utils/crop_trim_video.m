977547c9617b1408112d5adbf49f4b3c52dead99
Animadversio <binxu.wang@wustl.edu>
2020-11-08 11:31:37 -0600

    More stimuli for natural videos

--- utils/crop_trim_video.m
+++ utils/crop_trim_video.m
@@ -51,13 +51,25 @@ winopen(fullfile(vidroot,"bird2.avi"))
 %%
 vidroot = "N:\Stimuli\natural_animal_movies"; 
 frames = cropTrimVideo(vid, fullfile(vidroot,"bird1.avi"), 140, [340,150,450,450]);
-%%
-frames = cropTrimVideo(vid, fullfile(vidroot,"bird2.avi"), 201.5, [116.5100  248.5100  500  500]);
+frames = cropTrimVideo(vid, fullfile(vidroot,"bird2.avi"), 201.5, [116.5100  220  500  500]);
 frames = cropTrimVideo(vid, fullfile(vidroot,"bird3.avi"), 502.5, [400  220  500  500]);
 frames = cropTrimVideo(vid, fullfile(vidroot,"bird4.avi"), 600.5, [400  220  500  500]);
 frames = cropTrimVideo(vid, fullfile(vidroot,"bird5.avi"), 803.5, [350  220  500  500]);
 %%
-function frames = cropTrimVideo(vid, newvidpath, startTime, cropWdw)
+vidroot = "N:\Stimuli\natural_animal_movies"; 
+frames = cropTrimVideo(vid, fullfile(vidroot,"bird6.avi"), 2000, [280,250,450,450],true);
+%%
+frames = cropTrimVideo(vid, fullfile(vidroot,"bird7.avi"), 1300, [550  100  600  600],true);
+frames = cropTrimVideo(vid, fullfile(vidroot,"bird8.avi"), 1501.5, [600  220  500  500],true);
+frames = cropTrimVideo(vid, fullfile(vidroot,"bird9.avi"), 1600, [400  250  450  450],true);
+frames = cropTrimVideo(vid, fullfile(vidroot,"bird10.avi"), 1905, [1  220  500  500],true);
+frames = cropTrimVideo(vid, fullfile(vidroot,"bird11.avi"), 2539, [1  120  600  600],true);
+frames = cropTrimVideo(vid, fullfile(vidroot,"bird12.avi"), 2250, [350  220  500  500],true);
+%%
+%%
+%%
+function frames = cropTrimVideo(vid, newvidpath, startTime, cropWdw, doFrames)
+if nargin == 4, doFrames = true; end
 vid.CurrentTime = startTime;cropWdw = cropWdw;
 frames = {};
 for i = 1:120
@@ -71,12 +83,14 @@ for i = 1:numel(frames)
     vidnew.writeVideo(frames{i});
 end
 close(vidnew);
-% winopen(newvidpath)
+winopen(newvidpath)
+if doFrames
 pathparts = split(newvidpath,"\");
 nameparts = split(pathparts{end},".");
 mkdir(fullfile(pathparts{1:end-1},nameparts{1}))
 for i = 1:numel(frames)
     impath = fullfile(pathparts{1:end-1},nameparts{1},compose("%s_%03d.jpg",nameparts{1},i));
     imwrite(frames{i},impath)
+end
 end
 end
