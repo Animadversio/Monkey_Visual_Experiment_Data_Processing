29e6d58f93e56a74516bf763fc73eb2a9f741e0c
Animadversio <binxu.wang@wustl.edu>
2020-11-10 15:16:08 -0600

    Refactor The masking experiment analysis code and make it much more general

--- Masking_Analysis_Tuning.m
+++ Masking_Analysis_Tuning.m
@@ -1,31 +1,144 @@
-% Adapt the Masking Analysis Code 
-
-Animal = "Alfa"; Set_Path;
-ftr = find(contains(ExpRecord.ephysFN,"Alfa-29102020"));
-ExpRecord(ftr,:)
-[meta_new,rasters_new,~,Trials_new] = loadExperiments(ftr(4),Animal);
+clearvars -except Trials* meta* rasters* ExpRecord*
 %%
-rasters = rasters_new{1};
-meta = meta_new{1};
-Trials = Trials_new{1};
+% Adapt the Masking Analysis Code 
+Animal = "Alfa"; Set_Path;
+ftr = find(contains(ExpRecord.Exp_collection,'Masking'));  % "Alfa-06112020" "Alfa-04112020"
+ExpRecord(ftr,:)
+[meta_new,rasters_new,~,Trials_new] = loadExperiments(ftr(:),Animal);
+%%
+% resultroot = "OneDrive - Washington University in St. Louis";
+figroot = "E:\OneDrive - Washington University in St. Louis\VisualMask_PSTH";
+for Triali = 3:numel(meta_new)
+rasters = rasters_new{Triali};
+meta = meta_new{Triali};
+Trials = Trials_new{Triali};
 wdw = meta.rasterWindow;
+fprintf("Experiment: %s, %s:\n%s\n",meta.ephysFN,meta.expControlFN,meta.comments)
+% naming convention for the thing.
+fdrnm = figdir_naming(meta, ExpRecord);
+figdir = fullfile(figroot, fdrnm);
+if exist(figdir,'dir')
+    fprintf("Is %s OK?", fdrnm)
+%     keyboard
+end
+mkdir(figdir)
 %%
 % unit_name_arr = generate_unit_labels(meta.spikeID);
 % [activ_msk, unit_name_arr, unit_num_arr] = check_channel_active_label(unit_name_arr, meta.spikeID, rasters);
 unit_name_arr = generate_unit_labels_new(meta.spikeID, meta.unitID);
-% [activ_msk, unit_name_arr, unit_num_arr] = check_channel_active_label(unit_name_arr, meta.spikeID, rasters);
-
 imgnm_uniq = unique(Trials.imageName);
 imgmsks = cellfun(@(nm)contains(Trials.imageName,nm),imgnm_uniq,'Uni',0);
+% Parse the time interval between images trial by trial
+%% Parse the delays and ISIs. And print them out for better examination. 
+[pre_ISI, post_ISI, duration, preISI_msk, postISI_msk, dur_msk, preISI_arr, postISI_arr, dur_arr] = parser_stimuli_timing(Trials);
+fprintf("Experiment Configuration:\n")
+fprintf("Pre-Stimuli ISIs (med of category): %s \n",join(compose("%.1f(%d) ",preISI_arr',cellfun(@sum,preISI_msk)')));
+fprintf("Post-Stimuli ISIs (med of category): %s \n",join(compose("%.1f(%d) ",postISI_arr',cellfun(@sum,postISI_msk)')));
+fprintf("Stimuli Duration (med of category): %s \n",join(compose("%.1f(%d) ",dur_arr',cellfun(@sum,dur_msk)')));
+assert(numel(dur_arr)==1, "Multiple stimuli duration detected, modify your code!\n")
+%% Func of post onset
+clrseq = brewermap(numel(postISI_msk),'Spectral');
+smth = 9; dur = dur_arr(1); nonoverlap_bnd = 150;
+for iCh = 1:numel(meta.spikeID)
+figure(9);clf;hold on;set(9,'pos',[1000         168         586         810]);
+T = tiledlayout(numel(postISI_msk),1,'Padding','compact','TileSpacing','compact');
+preISI_far_msk = any(cat(1,preISI_msk{preISI_arr > nonoverlap_bnd}),1); % (preISI_msk{4} | preISI_msk{5} | preISI_msk{6})
+for subfi = 1:numel(postISI_msk)
+nexttile(subfi)
+msk = preISI_far_msk & dur_msk{1} & postISI_msk{subfi}; 
+plot(wdw(1)+1:wdw(2), movmean(mean(rasters(iCh,:,msk),3),smth), 'Color',[clrseq(1,:),1], 'LineWidth',2)
+xlim([wdw(1),wdw(2)])
+vline(0.0,'k-.');vline(dur,'k:'); vline(dur+postISI_arr(subfi),'r-.')
+if subfi ~= numel(postISI_msk), xticklabels([]);end
+end
+align_tile_ylim(T);
+xlabel("Time to Stimuli Onset")
+title(T, compose("PSTH as a Function of Next Onset\n%s Ch %s",Animal,unit_name_arr(iCh)))
+saveas(9,fullfile(figdir,compose("Post_onset_PSTH_allimg_Chan%s.png",unit_name_arr(iCh))))
+savefig(9,fullfile(figdir,compose("Post_onset_PSTH_allimg_Chan%s.fig",unit_name_arr(iCh))))
+end
+%% Func of Pre offset
+smth = 9; dur = dur_arr; nonoverlap_bnd = 250;
+for iCh = 1:numel(meta.spikeID)
+figure(10);clf;hold on;set(10,'pos',[1000         168         586         810]);
+T = tiledlayout(numel(preISI_msk),1,'Padding','compact','TileSpacing','compact');
+postISI_far_msk = any(cat(1, postISI_msk{postISI_arr > nonoverlap_bnd}),1); % (postISI_msk{4} | postISI_msk{5} | postISI_msk{6})
+for subfi = 1:numel(preISI_msk)
+nexttile(subfi)
+msk = postISI_far_msk & dur_msk{1} & preISI_msk{subfi}; 
+plot(wdw(1)+1:wdw(2), movmean(mean(rasters(iCh,:,msk),3),smth), 'Color',[clrseq(1,:),1], 'LineWidth',2)
+xlim([wdw(1),wdw(2)])
+vline(0.0,'k-.');vline(dur,'k:');vline( - preISI_arr(subfi),'r-.')
+if subfi ~= numel(preISI_msk), xticklabels([]);end
+end
+align_tile_ylim(T);
+xlabel("Time to Stimuli Onset")
+title(T, compose("PSTH as a Function of Next Onset\n%s Ch %s",Animal,unit_name_arr(iCh)))
+saveas(10,fullfile(figdir,compose("Pre_offset_PSTH_allimg_Chan%s.png",unit_name_arr(iCh))))
+savefig(10,fullfile(figdir,compose("Pre_offset_PSTH_allimg_Chan%s.fig",unit_name_arr(iCh))))
+% pause;
+end
+%% All Image plot separately pre offset
+clrmap = brewermap(numel(imgmsks), 'Set3');
+smth = 9;
+preISI_far_msk = any(cat(1, preISI_msk{preISI_arr > nonoverlap_bnd}),1);
+for iCh = 1:numel(meta.spikeID)
+figure(12);clf;hold on;set(12,'pos',[1000         168         586         810]);
+T = tiledlayout(numel(postISI_msk),1,'Padding','compact','TileSpacing','compact');
+for figi = 1:numel(postISI_msk)
+nexttile(figi);hold on
+for imgi = 1:numel(imgmsks)
+msk = preISI_far_msk & postISI_msk{figi} & dur_msk{1} & imgmsks{imgi}'; 
+plot(wdw(1)+1:wdw(2), movmean(mean(rasters(iCh,:,msk),3),smth), 'Color',[clrmap(imgi,:),0.5], 'LineWidth',2)
+end
+xlim([wdw(1),wdw(2)])
+vline(0.0,'k-.');vline(dur,'k:');vline(dur + postISI_arr(figi),'r-.');
+end
+align_tile_ylim(T);
+xlabel("Time to Stimuli Onset")
+title(T, compose("PSTH as a Function of Post Offset ISI\n%s Ch %s",Animal,unit_name_arr(iCh)))
+saveas(12, fullfile(figdir,compose("Post_onset_PSTH_imgsep_Chan%s.png",unit_name_arr(iCh))))
+savefig(12, fullfile(figdir,compose("Post_onset_PSTH_imgsep_Chan%s.fig",unit_name_arr(iCh))))
+end
 %%
-tOn_abs = nan(1,size(rasters,3));
-tOn_abs = nan(1,size(rasters,3));
-pre_offset = nan(1,size(rasters,3));
-post_onset = nan(1,size(rasters,3));
-duration = nan(1,size(rasters,3));
-for iTr = 1:size(rasters,3)
-curT0 = double(Trials.imageONtime(iTr));
-iInTrial = Trials.imageInTrial(iTr);
+smth = 9;
+postISI_far_msk = any(cat(1, postISI_msk{postISI_arr > nonoverlap_bnd}),1); % (postISI_msk{5} | postISI_msk{6})
+for iCh = 1:numel(meta.spikeID)
+figure(11);clf;hold on;set(11,'pos',[1000         168         586         810]);
+T = tiledlayout(numel(preISI_msk),1,'Padding','compact','TileSpacing','compact');
+for figi = 1:numel(preISI_msk)
+nexttile(figi);hold on
+for imgi = 1:numel(imgmsks)
+msk = preISI_msk{figi} & postISI_far_msk & dur_msk{1} & imgmsks{imgi}'; 
+plot(wdw(1)+1:wdw(2), movmean(mean(rasters(iCh,:,msk),3),smth), 'Color',[clrmap(imgi,:),0.5], 'LineWidth',2)
+end
+xlim([wdw(1),wdw(2)])
+vline(0.0,'k-.');vline(dur,'k:');vline(- preISI_arr(figi),'r-.');
+end
+align_tile_ylim(T);
+xlabel("Time to Stimuli Onset")
+title(T, compose("PSTH as a Function of Pre Onset ISI\n%s Ch %s",Animal,unit_name_arr(iCh)))
+saveas(11, fullfile(figdir,compose("Pre_offset_PSTH_imgsep_Chan%s.png",unit_name_arr(iCh))))
+savefig(11, fullfile(figdir,compose("Pre_offset_PSTH_imgsep_Chan%s.fig",unit_name_arr(iCh))))
+end
+
+end
+%% Collect Statics for this 
+
+
+%%
+[pre_ISI, post_ISI, duration, preISI_msk, postISI_msk, dur_msk, preISI_arr, postISI_arr, dur_arr] = parser_stimuli_timing(Trials);
+function [pre_ISI, post_ISI, duration, preISI_msk, postISI_msk, dur_msk, preISI_arr, postISI_arr, dur_arr] = parser_stimuli_timing(Trials)
+% Key function for the masking experiment analysis.
+trialNum = numel(Trials.trialNum);
+tOn_abs = nan(1,trialNum);
+pre_ISI = nan(1,trialNum);
+post_ISI = nan(1,trialNum);
+duration = nan(1,trialNum);
+for iTr = 1:trialNum
+curT0 = double(Trials.imageONtime(iTr)); % image onset time 
+iInTrial = Trials.imageInTrial(iTr); % image numbering in this RSVP trial
+
 iImgON = find(any(Trials.eventMarkers{iTr}(:,2) == (101:107),2));
 iImgOFF = find(any(Trials.eventMarkers{iTr}(:,2) == (201:207),2)); % Note this may be trial start code, not image off code
 % curT0 = Trials.eventMarkers{iTr}(iImgON(iInTrial),1);
@@ -38,94 +151,70 @@ tLastOff = max(tImgOFF(tImgOFF<0));
 tCurOff = min(tImgOFF(tImgOFF>0));
 tNextOn = min(tImgON(tImgON>0.1));
 if isempty(tNextOn), tNextOn = 600; end
-if isempty(tLastOff), tLastOff = -250; end
+if isempty(tLastOff), tLastOff = -500; end
 
-pre_offset(iTr) = 0 - tLastOff;
-post_onset(iTr) = tNextOn - tCurOff;
-duration(iTr) = tCurOff;
+pre_ISI(iTr) = 0 - tLastOff;
+post_ISI(iTr) = tNextOn - tCurOff;
+duration(iTr) = tCurOff; % Trials.imageOFFtime(iTr) - Trials.imageONtime(iTr);
 
 if tNextOn - tCurOff<0, keyboard;end
 end
-%%
-dur_msk = arrayfun(@(d) abs(duration - d)<2, [66.6], 'Uni', 0);
-preOff_msk = cellfun(@(wdw) (pre_offset > wdw(1)) & (pre_offset < wdw(2)), {[0,30],[30,65],[65,100],[100,150],[150,250],[250,inf]}, 'Uni', 0);
-postOn_msk = cellfun(@(wdw) (post_onset > wdw(1)) & (post_onset < wdw(2)), {[0,30],[30,65],[65,100],[100,150],[150,250],[250,inf]}, 'Uni', 0);
-%%
-clrseq = brewermap(numel(postOn_msk),'Spectral');
-figdir = "E:\OneDrive - Washington University in St. Louis\VisualMask_PSTH\2020-10-29-Alfa-01";
-mkdir(figdir)
-smth = 9; dur = 66.66666; 
-for iCh = 1:numel(meta.spikeID)
-figure(9);clf;hold on;set(9,'pos',[1000         168         586         810]);
-T = tiledlayout(numel(postOn_msk),1,'Padding','compact','TileSpacing','compact');
-for subfi = 1:numel(postOn_msk)
-nexttile(subfi)
-msk = (preOff_msk{4} | preOff_msk{5} | preOff_msk{6}) & dur_msk{1} & postOn_msk{subfi}; 
-plot(wdw(1)+1:wdw(2), movmean(mean(rasters(iCh,:,msk),3),smth),'Color',[clrseq(subfi,:),1],'LineWidth',2)
-vline(0.0,'k-.');vline(dur,'k:'); vline(dur+median(post_onset(postOn_msk{subfi})),'r-.')
-xlim([wdw(1),wdw(2)])
-if subfi ~= numel(postOn_msk), xticklabels([]);end
-end
-xlabel("Time to Stimuli Onset")
-title(T, compose("PSTH as a Function of Next Onset\n%s Ch %s",Animal,unit_name_arr(iCh)))
-saveas(9,fullfile(figdir,compose("Post_onset_PSTH_allimg_Chan%s.png",unit_name_arr(iCh))))
-savefig(9,fullfile(figdir,compose("Post_onset_PSTH_allimg_Chan%s.fig",unit_name_arr(iCh))))
-end
-%%
-smth = 9; dur = 66.66666; 
-for iCh = 1:numel(meta.spikeID)
-figure(10);clf;hold on;set(11,'pos',[1000         168         586         810]);
-T = tiledlayout(numel(preOff_msk),1,'Padding','compact','TileSpacing','compact');
-for subfi = 1:numel(preOff_msk)
-nexttile(subfi)
-msk = (postOn_msk{4} | postOn_msk{5} | postOn_msk{6}) & dur_msk{1} & preOff_msk{subfi}; 
-plot(wdw(1)+1:wdw(2), movmean(mean(rasters(iCh,:,msk),3),smth),'Color',[clrseq(subfi,:),1],'LineWidth',2)
-vline(0.0,'k-.');vline(dur,'k:');vline( - median(pre_offset(preOff_msk{subfi})),'r-.')
-xlim([wdw(1),wdw(2)])
-if subfi ~= numel(preOff_msk), xticklabels([]);end
-end
-xlabel("Time to Stimuli Onset")
-title(T, compose("PSTH as a Function of Next Onset\n%s Ch %s",Animal,unit_name_arr(iCh)))
-saveas(10,fullfile(figdir,compose("Pre_offset_PSTH_allimg_Chan%s.png",unit_name_arr(iCh))))
-savefig(10,fullfile(figdir,compose("Pre_offset_PSTH_allimg_Chan%s.fig",unit_name_arr(iCh))))
-pause;
-end
-%% All Image plot separately pre offset
-clrmap = brewermap(numel(imgmsks), 'Set3');
-smth = 9;
-for iCh = 1:numel(meta.spikeID)
-figure(11);clf;hold on;set(11,'pos',[1000         168         586         810]);
-T = tiledlayout(numel(preOff_msk),1,'Padding','compact','TileSpacing','compact');
-for figi = 1:numel(preOff_msk)
-nexttile(figi);hold on
-for imgi = 1:numel(imgmsks)
-msk = (preOff_msk{figi}) & dur_msk{1} & (postOn_msk{5} | postOn_msk{6}) & imgmsks{imgi}'; 
-plot(wdw(1)+1:wdw(2), movmean(mean(rasters(iCh,:,msk),3),smth),'Color',[clrmap(imgi,:),0.5],'LineWidth',2)
-end
-vline(0.0,'k-.');vline(dur,'k:');vline(-median(pre_offset(preOff_msk{figi})),'r-.');
-xlim([wdw(1),wdw(2)])
-end
-title(T, compose("PSTH as a Function of Previous Offset\n%s Ch %s",Animal,unit_name_arr(iCh)))
-saveas(11, fullfile(figdir,compose("Pre_offset_PSTH_imgsep_Chan%s.png",unit_name_arr(iCh))))
-savefig(11, fullfile(figdir,compose("Pre_offset_PSTH_imgsep_Chan%s.fig",unit_name_arr(iCh))))
-% pause
-end
-%%
-smth = 9;
-for iCh = 1:numel(meta.spikeID)
-figure(12);clf;hold on;set(12,'pos',[1000         168         586         810]);
-T = tiledlayout(numel(postOn_msk),1,'Padding','compact','TileSpacing','compact');
-for figi = 1:numel(postOn_msk)
-nexttile(figi);hold on
-for imgi = 1:numel(imgmsks)
-msk = (preOff_msk{4} | preOff_msk{5} | preOff_msk{6}) & dur_msk{1} & (postOn_msk{figi}) & imgmsks{imgi}'; 
-plot(wdw(1)+1:wdw(2), movmean(mean(rasters(iCh,:,msk),3),smth),'Color',[clrmap(imgi,:),0.5],'LineWidth',2)
-end
-vline(0.0,'k-.');vline(dur,'k:');vline(dur+median(post_onset(postOn_msk{figi})),'r-.');
-xlim([wdw(1),wdw(2)])
-end
-title(T, compose("PSTH as a Function of Previous Offset\n%s Ch %s",Animal,unit_name_arr(iCh)))
-saveas(12, fullfile(figdir,compose("Post_onset_PSTH_imgsep_Chan%s.png",unit_name_arr(iCh))))
-savefig(12, fullfile(figdir,compose("Post_onset_PSTH_imgsep_Chan%s.fig",unit_name_arr(iCh))))
 
+% Quantize the trials by the frame number (8.33333ms)
+delay_limit = 250;
+frameD = 100/12;
+preISI_qu = round(pre_ISI/frameD);
+postISI_qu = round(post_ISI/frameD);
+% Sort the Quantized trials. 
+preISI_categ = unique(round(pre_ISI(pre_ISI < delay_limit)/frameD)); 
+postISI_categ = unique(round(post_ISI(post_ISI < delay_limit)/frameD)); 
+% Create mask and array for it 
+preISI_msk = arrayfun(@(frN)preISI_qu==frN, preISI_categ, 'Uni',0); 
+preISI_msk{end+1} = pre_ISI >= delay_limit;
+postISI_msk = arrayfun(@(frN)postISI_qu==frN, postISI_categ, 'Uni',0); 
+postISI_msk{end+1} = post_ISI >= delay_limit;
+preISI_arr = cellfun(@(idx) median(pre_ISI(idx)), preISI_msk);
+postISI_arr = cellfun(@(idx) median(post_ISI(idx)), postISI_msk);
+
+dur_qu = round(duration / frameD);
+dur_categ = unique(dur_qu);
+dur_msk = arrayfun(@(frN) dur_qu==frN, dur_categ, 'Uni', 0);
+dur_arr = cellfun(@(idx) median(duration(idx)), dur_msk);
+% get rid of some minority 
+minor_categ = cellfun(@(msk)sum(msk)<6,dur_msk);
+dur_msk(minor_categ) = [];
+dur_arr(minor_categ) = [];
+% hand coded categories
+% preISI_msk = cellfun(@(wdw) (pre_ISI > wdw(1)) & (pre_ISI < wdw(2)), {[0,30],[30,65],[65,100],[100,150],[150,250],[250,inf]}, 'Uni', 0);
+% postISI_msk = cellfun(@(wdw) (post_ISI > wdw(1)) & (post_ISI < wdw(2)), {[0,30],[30,65],[65,100],[100,150],[150,250],[250,inf]}, 'Uni', 0);
 end
+
+function fdrnm = figdir_naming(meta, ExpRecord)
+Animal = meta.ephysFN(1:4);
+expday = datetime(meta.expControlFN(1:6),'InputFormat','yyMMdd');
+chari = strfind(meta.expControlFN, 'Masking');
+rowi_day = find(contains(ExpRecord.expControlFN, meta.expControlFN(1:chari+6)));
+rowi = find(strcmp(ExpRecord.expControlFN, meta.expControlFN));
+numday = find(rowi_day == rowi); % the numbering of experiment among exp in that day. 
+fdrnm = compose("%s-%s-%02d",datestr(expday,'yyyy-mm-dd'), Animal, numday);
+fprintf("Proposed folder name %s\n",fdrnm);
+end
+
+function align_tile_ylim(T)
+ax_arr = get(T,'Children');
+ylim_arr = [];
+for i = 1:numel(ax_arr)
+    ylim_arr(i,:) = ylim(ax_arr(i));
+end
+YLIM(1) = min(ylim_arr(:,1)); YLIM(2) = max(ylim_arr(:,2));
+for i = 1:numel(ax_arr)
+    ylim(ax_arr(i), YLIM);
+    alllines = allchild(ax_arr(i));
+    for ln = alllines' 
+    if strcmp(ln.Tag,'vline')
+        ln.YData = YLIM; % Change the y limit for the Vlines
+    end
+    end
+end
+end
+
