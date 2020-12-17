175330297eaed88e7ab5842e102d9ef47603996a
Animadversio <binxu.wang@wustl.edu>
2020-12-16 18:39:54 -0600

    Fit Kent distribution again

--- Manif_Fit_Popul.m
+++ Manif_Fit_Popul.m
@@ -1,4 +1,4 @@
-Animal="Alfa"; Set_Path;
+Animal="Beto"; Set_Path;
 mat_dir = "O:\Mat_Statistics";
 tabdir = "O:\Manif_Fitting\popstats";
 % load(fullfile(Matdir, "Beto_ManifPopDynamics.mat"))
@@ -7,7 +7,7 @@ load(fullfile(mat_dir,Animal+"_Manif_stats.mat"),'Stats')
 %%
 addpath D:\Github\Fit_Spherical_Tuning
 addpath e:\Github_Projects\Fit_Spherical_Tuning
-pool = parpool(4);
+% pool = parpool(6);
 T00 = tic;
 parfor Expi = 1:numel(Stats)
 % Basic information to collect into the Stats
@@ -30,61 +30,32 @@ actmap_mean = arrayfun(@(iCh)cellfun(@(A)...
 toc
 KentStats_exp = cellfun(@(actmap)fit_Kent_stats(actmap),actmap_mean);
 anovaStats_exp = cellfun(@anova_cells,actmap_col);
-keyboard
 toc
 anovaStats_tab = struct2table(anovaStats_exp);
 KentStats_tab = struct2table(KentStats_exp);
-StatsTab = [Animal_tab, Expi_tab, unitstr_tab, unit_tab, chan_tab, prefchan_tab, space_tab, anovaStats_tab, smthStats_tab];
+StatsTab = [Animal_tab, Expi_tab, unitstr_tab, unit_tab, chan_tab, prefchan_tab, space_tab, anovaStats_tab, KentStats_tab];
 writetable(StatsTab,fullfile(tabdir,compose("%s_Exp%d_sp%d.csv",Animal,Expi,si)));
 % StatsTab_sum = [StatsTab_sum;StatsTab];
 % disp(size(StatsTab_sum))
 end
 end
 toc(T00);
-
+StatsTab_sum = [];
+for Expi = 1:numel(Stats)
+for si = 1:MapVarStats(Expi).manif.subsp_n
+curtab = readtable(fullfile(tabdir,compose("%s_Exp%d_sp%d.csv",Animal,Expi,si)));
+StatsTab_sum = [StatsTab_sum;curtab];
+end
+end
+writetable(StatsTab_sum,fullfile(tabdir,compose("%s_Exp_all_KentStat_pole.csv",Animal)));
 %%
-Kent_stats = repmat(struct(), 1, length(Stats));
-for i = 1:length(Stats)
-% there can be multiple units, or multiple subspace, so we have this cell
-% array
-stats_grid = cell(numel(Stats(i).units.pref_chan_id), Stats(i).manif.subsp_n);
-scr_stats_grid = cell(numel(Stats(i).units.pref_chan_id), Stats(i).manif.subsp_n);
-subsp_str = ['PC23',"PC4950",'RND12'];
-subsp_axis = ["PC2","PC3";"PC49","PC50";"RND1","RND2"];
-for subsp_i = 1:Stats(i).manif.subsp_n
-imgnm_grid = cellfun(@(idx) unique([Stats(i).imageName(idx)]), Stats(i).manif.idx_grid{subsp_i});
-psths = Stats(i).manif.psth{subsp_i};
-fprintf(subsp_str{subsp_i}+"\n")
-for uniti = 1:1:numel(Stats(i).units.pref_chan_id)
-channel_j = Stats(i).units.pref_chan_id(uniti);
-chan_label_str = sprintf("%s Exp%d Channel %s", Stats(i).Animal, Stats(i).Expi, ...
-            Stats(i).units.unit_name_arr{channel_j});
 
-mean_score_map = cellfun(@(psth) mean(psth(uniti, 51:200, :),[2,3])-mean(psth(uniti, 1:40, :),[2,3]), psths);
-[Parameter, gof] = fit_Kent(mean_score_map);
-scr_stats_grid{uniti, subsp_i} = gof;
-% V = coeffvalues(Parameter);
-% CI = confint(Parameter);
-% param_str = sprintf("theta=%.2f (%.2f, %.2f)  phi=%.2f (%.2f, %.2f)\n psi=%.2f (%.2f, %.2f)  A=%.2f (%.2f, %.2f)\n kappa=%.2f (%.2f, %.2f)  beta=%.2f (%.2f, %.2f) ", ...
-%                 V(1), CI(1,1), CI(2,1), V(2), CI(1,2), CI(2,2), V(3), CI(1,3), CI(2,3), V(6), CI(1,6), CI(2,6), V(4), CI(1,4), CI(2,4), V(5), CI(1,5), CI(2,5));
-mean_act_map = cellfun(@(psth) mean(psth(uniti, 51:200, :),[2,3]), psths);
-[Parameter, gof] = fit_Kent(mean_act_map);
-stats_grid{uniti, subsp_i} = gof;
-V = coeffvalues(Parameter);
-CI = confint(Parameter);
-param_str = sprintf("theta=%.2f (%.2f, %.2f)  phi=%.2f (%.2f, %.2f)\n psi=%.2f (%.2f, %.2f)  A=%.2f (%.2f, %.2f)\n kappa=%.2f (%.2f, %.2f)  beta=%.2f (%.2f, %.2f) ", ...
-                V(1), CI(1,1), CI(2,1), V(2), CI(1,2), CI(2,2), V(3), CI(1,3), CI(2,3), V(6), CI(1,6), CI(2,6), V(4), CI(1,4), CI(2,4), V(5), CI(1,5), CI(2,5));
-fprintf(chan_label_str+"\n "+param_str+"\n")
-end
-end
-Kent_stats(i).act_fit = stats_grid;
-Kent_stats(i).scr_fit = scr_stats_grid;
-end
-toc
-
-%%
 
 function S = fit_Kent_stats(funcval)
+% Mean fun
+funcval(:,1) = mean(funcval(:,1));
+funcval(:,end) = mean(funcval(:,end));
+
 [Parameter, gof] = fit_Kent(funcval);
 S.sse = gof.sse;
 S.R2 = gof.rsquare;
