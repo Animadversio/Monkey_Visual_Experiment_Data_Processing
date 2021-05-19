8f30c43eb0b7b23149b24233b6a0e94e347c08d5
Animadversio <binxu.wang@wustl.edu>
2021-05-19 03:21:50 -0500

    Add cosine exp analysis suite and visualization

--- Evol_Cosine_Analysis.m
+++ Evol_Cosine_Analysis.m
@@ -344,7 +344,7 @@ xlabel("Channels");ylabel("Activation");
 title(compose("PopRepr Evol\n%s",explabel),'interpreter','none')
 end
 
-function chanmsk = parse_mode2mask(mode, chan_arr, unit_arr)
+function [chanmsk, targ_area] = parse_mode2mask(mode, chan_arr, unit_arr)
 % Parse the score_mode string into a channel mask of which channels are
 % used in the exp.
 % chanmsk = parse_mode2mask("corr_V4IT");
@@ -352,18 +352,23 @@ MAXUNUM = 4;
 if nargin ==1, chan_arr=[1:64]'; unit_arr = 0:MAXUNUM; end
 chanMat = repmat(chan_arr,1,numel(unit_arr));
 unitMat = repmat(unit_arr,numel(chan_arr),1);
-if ~contains(mode,["V1","V4","IT"])
+targ_area = "";
+if ~contains(mode,["V1","V4","IT"]) || contains(mode,"All")
     chanmsk = ones(size(chanMat),'logical');
+    targ_area = "V1V4IT";
 else
     chanmsk = zeros(size(chanMat),'logical');
     if contains(mode,"IT")
         chanmsk = chanmsk | ((chan_arr<=32));
+        targ_area=targ_area+"IT";
     end
     if contains(mode,"V4")
         chanmsk = chanmsk | ((chan_arr>=49));
+        targ_area=targ_area+"V4";
     end
     if contains(mode,"V1")
         chanmsk = chanmsk | ((chan_arr<49) & (chan_arr>32));
+        targ_area=targ_area+"V1";
     end
 end
 end
