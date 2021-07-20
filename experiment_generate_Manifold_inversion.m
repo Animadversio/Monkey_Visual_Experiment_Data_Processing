0ce6e3b665a76513a92322b46748e42ae13ccfd4
Olivia <poncelaboratory@gmail.com>
2021-07-20 18:25:29 -0500

    add functionality to change scope

--- experiment_generate_Manifold_inversion.m
+++ experiment_generate_Manifold_inversion.m
@@ -5,21 +5,25 @@ global newimg_dir
 %%
 
 %% Spherical interpolation
-manifold_root = "S:\Manifold_Demo";
-imgname = "Banana";
-orig_img = imread("N:\Stimuli\2021-ProjectPFC\2021-Selectivity\bigSet\fruit (8).jpg");
+manifold_root = "C:\Users\ponce\Documents\monkeylogic2\selectivityBasic\manifold";
+imgname = "Apple";
+orig_img = imread("C:\Users\ponce\Documents\monkeylogic2\selectivityBasic\catsA\fruit (1).jpeg");
 target_img = imresize(orig_img, [256,256]);
 newimg_dir = fullfile(manifold_root, compose("%s_manifold",imgname));
 mkdir(newimg_dir);fprintf("Save new images to folder %s\n", newimg_dir);
-[code_invert, img_fit, loss] = GAN_invert_fun(G, target_img, 200);
+[code_invert, img_fit, loss] = GAN_invert_fun(G, target_img, 300);
 figure;imshow(imtile({target_img,img_fit}))
 code_invert = double(code_invert);
 %%
 rand_tanvec2 = orthonormalize2vect(code_invert, randn(2, 4096));
-[img_list] = explore_from_code(G,code_invert, rand_tanvec2, "RND12", ["RND1","RND2"]);
+[img_list] = explore_from_code(G,code_invert, rand_tanvec2, "RND12", ["RND1","RND2"],148,9);
 %%
 EIG_tanvec2 = orthonormalize2vect(code_invert, eigvect(:, 1:2)');
-[img_list] = explore_from_code(G,code_invert, EIG_tanvec2, "eig12", ["eig1","eig2"]);
+[img_list] = explore_from_code(G,code_invert, EIG_tanvec2, "eig12", ["eig1","eig2"],30,7);
+%%
+EIG_tanvec2 = orthonormalize2vect(code_invert, eigvect(:, 1:2)');
+[img_list] = explore_from_code(G,code_invert, EIG_tanvec2, "eig12", ["eig1","eig2"],30,7);
+
 function vects = orthonormalize2vect(centvect, vects)
 % tmp = orthonormalize2vect([1,zeros(1,4095)],randn(5,4096));
 % tmp = orthonormalize2vect(randn(5,4096));
@@ -38,10 +42,10 @@ end
 % rand_vec2(2,:) = rand_vec2(2,:) - (rand_vec2(2,:) * rand_vec2(1,:)') * rand_vec2(1,:) / vecnorm(rand_vec2(1,:),2,2).^2;
 % rand_vec2 = rand_vec2 ./ vecnorm(rand_vec2,2,2); %np.sqrt((rand_vec2**2).sum(axis=1))[:, np.newaxis]
 
-function [img_list] = explore_from_code(G, code_invert, tang_vecs, space_str, axes_str)
+function [img_list] = explore_from_code(G, code_invert, tang_vecs, space_str, axes_str, ANGLE_SPAN, imgN_per_arc)
 global newimg_dir
-ANGLE_SPAN = 180;
-imgN_per_arc = 11; 
+if nargin<=5, ANGLE_SPAN = 180; end
+if nargin<=6, imgN_per_arc = 11; end
 assert(length(code_invert)==4096)
 code_invert = reshape(code_invert,1,4096);
 if size(tang_vecs,1)==4096
@@ -57,8 +61,8 @@ basis1 = code_invert / sphere_norm;
 basis = [basis1; tang_vecs];
 fprintf("Generating images on %s sphere (radius = %.1f)\n", space_str, sphere_norm)
 img_list = {};
-for j = -5:5
-    for k = -5:5
+for j = -(imgN_per_arc-1)/2:(imgN_per_arc-1)/2
+    for k = -(imgN_per_arc-1)/2:(imgN_per_arc-1)/2
         theta = Theta_step * j / 180 * pi;
         phi = Phi_step * k / 180 * pi;
         code_vec = [cos(theta)*cos(phi),...
@@ -68,11 +72,11 @@ for j = -5:5
         img = G.visualize(code_vec);
         img_list{end+1} = img;
         imwrite(img, fullfile(newimg_dir, compose("norm_%d_%s_%d_%s_%d.jpg", ...
-            int32(sphere_norm), axes_str(1), Theta_step * j, axes_str(2), Phi_step* k)));
+            int32(sphere_norm), axes_str(1), int32(Theta_step * j), axes_str(2), int32(Phi_step* k))));
     end
 end
-mtg = imtile(img_list,'GridSize',[11,11], 'BorderSize',4,'ThumbnailSize',[256,256]);
-imwrite(mtg,fullfile(newimg_dir,compose("%s_Montage.png", space_str)));
+mtg = imtile(img_list,'GridSize',[imgN_per_arc,imgN_per_arc], 'BorderSize',4,'ThumbnailSize',[256,256]);
+imwrite(mtg,fullfile(newimg_dir,compose("norm_%d_%s_Montage.png", int32(sphere_norm), space_str)));
 save(fullfile(newimg_dir, compose("%s_tan_vect_data.mat", space_str)), ...
     "code_invert", "tang_vecs", "sphere_norm", "Theta_step", "Phi_step");
 end
