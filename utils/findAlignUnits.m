95b5dc44a8d405e042fcccf1ee769a46798d05cf
binxu.wang <binxu.wang@wustl.edu>
2022-03-13 22:32:36 -0400

    find units that aligns in two experiments (stats file)

---
+++ utils/findAlignUnits.m
@@ -0,0 +1,20 @@
+%% Align RFs for CosineEvolution and RFmapping 
+function Rchans = findAlignUnits(srcchans,CStat,RFStat)
+% This utils function find the unit indices that match the srcchans in
+% source experiments in he target expeirments. 
+% 
+% srcchans: a list of indices 
+% CStat: Stats for the source exp (like an evolution or cosine evolution)
+% RFStat: Stats for the target exp (like an RF mapping exp)
+if islogical(srcchans), 
+    srcchans=find(srcchans); 
+end
+Rchans = [];
+for iCh = reshape(srcchans,1,[])
+    Cchan = CStat.meta.spikeID(iCh);
+    Cunit = CStat.meta.unitID(iCh);
+    Rch = find(RFStat.meta.spikeID==Cchan &...
+               RFStat.meta.unitID==Cunit );
+    Rchans = [Rchans; Rch];
+end
+end
